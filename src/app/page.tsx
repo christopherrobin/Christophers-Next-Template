1d1caa65922f782650e67814090ad643724da942
Christopher Robin Reynolds <christopherrobinreynolds@gmail.com>
2025-04-27 05:31:59 -0400

    nerd icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { ArrowRightCircleIcon } from '@heroicons/react/20/solid'
 import { RocketLaunchIcon } from '@heroicons/react/24/outline'
-import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
 import React, { useEffect } from 'react'
@@ -20,29 +19,26 @@ export default function Home() {
   }, [status, router])
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen px-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] fadeIn">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          src="/plant.svg"
-          alt="Plant Logo"
-          width={180}
-          height={180}
-          className="mx-auto w-[180px] h-[180px]"
-          priority
-        />
-        <h1 className="text-2xl md:text-5xl font-bold text-green-700">
-          Howdy.
-        </h1>
+    <div className="grid grid-rows-[10px_1fr_20px] items-center justify-items-center min-h-screen px-6 gap-16 fadeIn">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start sm:w-full md:w-2/3 text-center sm:text-left">
+        <h1>Howdy.</h1>
+        <h2>Christophers-Next-Template</h2>
+        <p>
+          This is a template for Next.js applications with TypeScript, Tailwind
+          CSS, and NextAuth.js. It includes a simple authentication flow, a
+          protected dashboard, and reusable components to kickstart your
+          project.
+        </p>
         <Button
           href="/join"
-          className="self-center mt-2"
+          className="self-center mt-2 w-full"
           endIcon={<RocketLaunchIcon className="w-5 h-5" />}
         >
           Join Us
         </Button>
         <Button
           href="/sign-in"
-          className="self-center"
+          className="self-center w-full"
           endIcon={<ArrowRightCircleIcon className="w-5 h-5 text-green-700" />}
           ghost
         >

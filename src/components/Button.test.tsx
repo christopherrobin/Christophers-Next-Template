d4495facdd7683cb73ecefeb61c7dcb027a29a0e
Christopher Robin Reynolds <christopherrobinreynolds@gmail.com>
2026-04-26 16:40:27 -0400

    fix: test + colocate

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import React from 'react'
 
-import { Button } from '../src/components/Button'
+import { Button } from './Button'
 
 const icon = <svg data-testid="icon" />
 
